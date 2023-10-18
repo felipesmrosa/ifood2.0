1dffc12742dad3200dc56cd4e009af24851343a2
Felipe Miranda <felipem04.r@gmail.com>
2023-10-18 17:54:23 -0300

    Update carrinho.js

---
+++ js/carrinho.js
@@ -0,0 +1,10 @@
+//array que fica no localStorage
+//botão addEventListener (click, function {}) (Adicionar ao carrinho)
+// const addCart = document.querySelectorAll('.adicionarAoCarrinho');
+
+// addCart.forEach(button => {
+//     button.addEventListener('click', () => {
+//         const pegaInfo = document.querySelector('.restaurantes')
+//         console.log(pegaInfo)
+//     });
+// });
